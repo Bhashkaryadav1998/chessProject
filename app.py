b9bc5a4c4fcebd809601d854986ddc4c7e0a5b5c
Bhashkaryadav1998 <bhashkaryadav1998@gmail.com>
2021-05-14 21:07:34 +0530

    commit 3.7

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request, json, redirect
 import os
 
 from werkzeug.utils import secure_filename
@@ -7,17 +7,31 @@ import tensorflow_chessbot as chess
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static/img/uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+host = 'localhost'
+app.logger.info('Server started')
+
 
 @app.route('/')
 def hello_world():
+    app.logger.info('index.html loaded')
     return render_template('index.html')
 
 
 @app.route('/upload',methods=['POST'])
 def chessImage():
+    app.logger.info('Received request on upload page')
+    if 'file' not in request.files:
+        app.logger.error('No File part received')
+        return redirect(request.url)
     file = request.files['file']
+    if file.filename == '':
+        app.logger.error('No image received for analysis')
+        return redirect(request.url)
+    app.logger.info('Image received for fen conversion')
     flip=request.form['flip']
     filename = secure_filename(file.filename)
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    app.logger.info('Analysis started on Image')
     fen=chess.main('static/img/uploads/'+filename,flip)
+    app.logger.info('Analysis finished and fen is sent')
     return json.dumps(fen)
